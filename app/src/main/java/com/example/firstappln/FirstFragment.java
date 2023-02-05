fbdf2332253e2a6117ecdb06d23d5462cdd5d471
disguisedRabbit123 <omkarpednekar1@gmail.com>
2023-02-05 19:44:40 +0530

    Initial commit.

---
+++ app/src/main/java/com/example/firstappln/FirstFragment.java
@@ -0,0 +1,72 @@
+package com.example.firstappln;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.firstappln.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.randomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int currentCount = Integer.parseInt(binding.textviewFirst.getText().toString());
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(FirstFragmentDirections.actionFirstFragmentToSecondFragment(currentCount));
+            }
+        });
+
+        binding.toastButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast myToast = Toast.makeText(getActivity(), R.string.toast_text, Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
+
+        binding.countButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                countMe(view);
+            }
+        });
+    }
+
+    private void countMe(View view)
+    {
+        String countString = binding.textviewFirst.getText().toString();
+        Integer count = Integer.parseInt(countString);
+        count++;
+        binding.textviewFirst.setText(count.toString());
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
---
+++ app/src/main/java/com/example/firstappln/SecondFragment.java
@@ -0,0 +1,78 @@
+package com.example.firstappln;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.firstappln.databinding.FragmentSecondBinding;
+
+import java.util.Random;
+
+
+public class SecondFragment extends Fragment {
+
+    private FragmentSecondBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Integer count = SecondFragmentArgs.fromBundle(getArguments()).getMyArg();
+        String countText = getString(R.string.str_random_header, count);
+        TextView headerView = view.getRootView().findViewById(R.id.txt_random_header);
+        headerView.setText(countText);
+
+        Random random = new java.util.Random();
+        Integer randomNumber = 0;
+        if (count > 0) {
+            randomNumber = random.nextInt(count + 1);
+        }
+
+        TextView randomView = view.getRootView().findViewById(R.id.txt_random);
+        randomView.setText(randomNumber.toString());
+
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+
+        binding.btnSecondRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random random = new java.util.Random();
+                Integer randomNumber = 0;
+                if (count > 0) {
+                    randomNumber = random.nextInt(count + 1);
+                }
+
+                TextView randomView = view.getRootView().findViewById(R.id.txt_random);
+                randomView.setText(randomNumber.toString());
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
